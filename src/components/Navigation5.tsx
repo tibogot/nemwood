74c49ba79901e0645bb277d30eb3f47882ef27f5
tibogot <tibogotbeatz@gmail.com>
2025-09-02 19:44:53 +0200

    ter

--- src/components/Navigation5.tsx
+++ src/components/Navigation5.tsx
@@ -575,7 +575,7 @@ export default function Navigation() {
           <div
             className={`flex w-full ${
               isMobile
-                ? "flex-1 flex-col items-center justify-center space-y-8 py-8"
+                ? "flex-1 flex-col items-center justify-center space-y-6 py-4"
                 : "flex-row items-end justify-start gap-2 pb-8 md:gap-8"
             }`}
             onMouseLeave={() => setHoveredIndex(null)}
