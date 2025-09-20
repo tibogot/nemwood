f54cc746e0fc3a456149f3aa354026ad9940960a
tibogot <tibogotbeatz@gmail.com>
2025-09-20 18:22:53 +0200

    p

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,7 +5,7 @@ import { ArrowRight } from "lucide-react";
 
 export default function NotFound() {
   return (
-    <main className="bg-secondary text-primary flex h-screen flex-col items-center justify-center px-4 md:px-8">
+    <main className="bg-secondary text-primary flex min-h-[100svh] flex-col items-center justify-center px-4 md:px-8">
       {/* Background Image */}
       {/* <div className="absolute inset-0 z-0">
         <Image
