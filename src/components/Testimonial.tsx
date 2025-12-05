027976140d6811d2dac4c5f332c0b0949b3a507f
tibogot <tibogotbeatz@gmail.com>
2025-12-05 03:52:49 +0100

    gds

--- src/components/Testimonial.tsx
+++ src/components/Testimonial.tsx
@@ -145,7 +145,7 @@ export default function Testimonial() {
           loading="lazy"
         />
         <AnimatedText delay={0.0} stagger={0.3}>
-          <h2 className="font-ITCGaramondN relative text-5xl leading-tight text-[#fffcf5] md:text-7xl">
+          <h2 className="font-ITCGaramondN relative mt-16 text-5xl leading-tight text-[#fffcf5] md:mt-24 md:text-7xl">
             Témoignages
           </h2>
         </AnimatedText>
